e369af3c5253068e082124c44770885b5c491774
Salomone Baquis <salomone.l.baquis@gmail.com>
2018-12-22 22:50:20 -0500

    delay working now

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -1,13 +1,15 @@
 var cellHeight = 100
 var cellWidth = 100
-var windowWidth, windowHeight
-var coords
+var windowWidth, 
+		windowHeight,
+		coords
+
 var gridCanvas = document.querySelector("#grid")
 var circleCanvas = initCircleCanvas()
 var circleCtx = circleCanvas.getContext("2d")
-var circleData
-
+var count = 0
 init()
+runAnimation()
 
 window.addEventListener("resize", init)
 
@@ -17,23 +19,24 @@ function init() {
 	;[gridCanvas, circleCanvas].forEach(canvas => {
 		canvas.setAttribute("width", windowWidth)
 		canvas.setAttribute("height", windowHeight)
-	})	
-	drawGrid(generateCoords)
-	getCircleData(test)
+	})
+	coords = generateCoords()
+	drawGrid(coords)	
 }
 
 function generateCoords() { // Create coordinate board
 	coords = []
 	for (let i = 0; i < windowWidth; i++) {
 		if (i % cellWidth === 0) {
-			coords.push({ x: i, y: 0, xt: i, yt: windowHeight, d: "vertical" })
+			coords.push({ x: i, y: 0, xt: i, yt: windowHeight, start: "y", target: windowHeight })
 		}
 	}
 	for (let i = 0; i < windowHeight; i++) {
 		if (i % cellHeight === 0) {
-			coords.push({ x: 0, y: i, xt: windowWidth, yt: i, d: "horizontal" })
+			coords.push({ x: 0, y: i, xt: windowWidth, yt: i, start: "x", target: windowWidth })
 		}
 	}
+	console.log(coords)
 	return coords
 }
 
@@ -45,8 +48,7 @@ function initCircleCanvas() { // Create + append circle canvas
 	return circleCanvas
 }
 
-function drawGrid(generateCoords) {
-	coords = generateCoords()
+function drawGrid(coords) {
 	var ctx = gridCanvas.getContext("2d")
 
 	ctx.lineWidth = 0.4
@@ -60,261 +62,62 @@ function drawGrid(generateCoords) {
 		ctx.closePath()
 	})
 }
-function getCircleData(callback) {
-	var data = coords[Math.floor(Math.random() * coords.length)]
-	var circle = data.d === "horizontal" 
-		? { x: data.x, y: data.y, target: data.xt, direction: "horizontal", start: "x" } 
-		: { x: data.x, y: data.y, target: data.yt, direction: "vertical", start: "y" }
+
+function outer() {
+	var start = performance.now()
+	var word = "thing"
 	
-	circle.r = Math.floor(Math.random() * (50 - 10 + 1) + 10);
-	circleData = circle
-	callback()
+	requestAnimationFrame(function(timestamp) {
+		inner(timestamp, start, word)
+  })
 }
-function test() {	
-	if (circleData[circleData.start] < circleData.target) {	
-		drawCircle()
-		requestAnimationFrame(test)
-	}
+function inner(timestamp, start, word) {
+	if (timestamp - start > 5000) {
+		console.log(word)
+		start = timestamp
+   }
+	requestAnimationFrame(function(timestamp) {
+		inner(timestamp, start, word)
+    })
+}
+function circleData() {
+	var circleData = coords[Math.floor(Math.random() * coords.length)]
+	circleData.r = Math.floor(Math.random() * (50 - 10 + 1) + 10)
+	return circleData	
 }
 
-function drawCircle() {	
+function runAnimation() {
+	circleData = circleData()
+	var start = performance.now()
+
+	requestAnimationFrame(function(timestamp) {
+		animate(timestamp, start, circleData)
+  })
+}
+function animate(timestamp, start, circleData) {
+	drawCircle(circleData)
+	var delta = timestamp - start
+	if (delta > 6000) {
+		start = timestamp
+
+		if (circleData[circleData.start] < circleData.target) {
+			drawCircle(circleData)
+		} else {
+			circleData = circleData()
+			drawCircle(circleData)
+		}		
+	}
+	requestAnimationFrame(function(timestamp) {
+		animate(timestamp, start, circleData)
+  })
+}
+
+function drawCircle(circleData) {	
 	circleCtx.clearRect(0, 0, circleCanvas.width, circleCanvas.height)
 	circleCtx.fillStyle = "rgba(0, 255, 255, 0.4)";
 	circleCtx.beginPath();	
 	circleCtx.arc(circleData.x, circleData.y, circleData.r, 0, 2 * Math.PI)
 	circleCtx.fill();
 	circleCtx.closePath();
-	circleData[circleData.start]+=8
-	console.log(circleData)
+	circleData[circleData.start]+=5	
 }
-
-// function animate(x,y,r,target,ctx, canvas, callback) {
-// 	var xPos = x
-// 	var yPos = y
-// 	var radius = r
-// 	var target = target
-// 	var canvas = canvas
-// 	var callback = callback
-
-// 	function callback() {
-// 			ctx.clearRect(0, 0, canvas.width, canvas.height)
-// 			console.log(xPos, yPos)
-// 			drawCircle(xPos, yPos, radius, ctx, canvas)
-// 			x++
-// 			// requestAnimationFrame(function() {
-// 			// 	animate(xPos,yPos,radius,target,ctx, canvas, callback)
-// 			// })
-// 			return
-// 	}	
-// 	if (x < target) {	
-// 		return callback()
-// 	}
-// 	return "blah"
-// }
-
-// var count = 1
-// var end = 20
-// function outer(count, end) {	
-// 	if (end < 321) {
-// 		increment()
-//     }
-	
-// }
-
-
-// function increment() {
-// 	console.log("count is: ", count)
-// 	count++
-// 	if (count < end) {
-// 		requestAnimationFrame(increment)
-//     } else {
-// 		outer(count+=200, end+=300) 
-//     } 
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const viewportWidth = window.innerWidth
-// const viewportHeight = window.innerHeight
-// const animationDuration = 3000
-// const color = createColor()
-
-// const cellDimensions = function() {
-// 	return {
-// 		cellHeight: 100,
-// 		cellWidth: 100
-// 	}
-// }
-
-// function createGridData() {
-// 	const data = []
-// 	const { cellHeight, cellWidth } = cellDimensions()
-
-// 	const rows = Math.ceil(viewportHeight / cellHeight )
-// 	const columns = Math.ceil(viewportWidth / cellWidth)	
-
-// 	let xPos = 1
-// 	let yPos = 1
-
-// 	for (let i = 0; i < rows; i++) {
-// 		data.push([])
-// 		for (let j = 0; j < columns; j++) {
-// 			let square = {
-// 				x: xPos, 
-// 				y: yPos,
-// 				cellWidth: cellWidth, 
-// 				cellHeight: cellHeight
-// 			}
-// 			data[i].push(square)
-// 			xPos += cellWidth
-// 		}
-// 		xPos = 1
-// 		yPos += cellHeight
-// 	}		
-// 	return data
-// }
-
-// const gridData = createGridData()
-
-// // Make the grid
-// const grid = d3.select("#grid")
-// 	.attr("width", `${viewportWidth}px`)
-// 	.attr("height", `${viewportHeight}px`)
-
-// // Make the rows
-// const row = grid.selectAll(".row")
-// 	.data(gridData)
-// 	.enter().append("g")
-// 	.attr("class", "row")	
-
-// // Make the squares
-// const square = row.selectAll(".square")
-// 	.data(function(d) { return d })
-// 	.enter().append("rect")
-// 	.attr("class", "square")
-// 	.attr("x", function(d) { return d.x })
-// 	.attr("y", function(d) { return d.y })	
-// 	.attr("width", function(d) { return d.cellWidth })
-// 	.attr("height", function(d) { return d.cellHeight })
-// 	.style("fill", "transparent")
-// 	.style("stroke", "#eee")	
-
-// // Get the max and min coordinate values on the board
-// function getGridBoundaries(gridData) {
-// 	const gridDataLength = gridData.length-1
-
-// 	return {
-// 		minX: gridData[0][0].x,
-// 		minY: gridData[0][0].y,
-// 		maxX: gridData[0][gridData[0].length-1].x,
-// 		maxY: gridData[gridDataLength][gridData[gridDataLength].length-1].y
-// 	}
-// }
-
-// const gridBoundaries = getGridBoundaries(gridData)
-
-// function createParticleData() {
-// 	// Create a filtered, flattened array of square coordinates
-// 	// and reduce to a single object
-// 	const filteredData = gridData.map(row => {
-// 		row = row.filter(data => {
-// 			return data.x === gridBoundaries.minX 
-// 				|| data.x === gridBoundaries.maxX 
-// 				|| data.y === gridBoundaries.minY 
-// 				||  data.y === gridBoundaries.maxY
-// 		})
-// 		return row.map(square => {
-// 			return { x: square.x, y: square.y }
-// 		})
-// 	})
-// 	.reduce((accumulator, currentValue) => {
-// 		return accumulator.concat(currentValue)
-// 	})	
-// 	return [filteredData[Math.floor(Math.random() * filteredData.length)]]
-// }	
-
-// // Get the key that we'll use to calculate the transform
-// function getTransformKey(x,y,middle) {
-// 	const keyObj = {
-// 		[`${y === gridBoundaries.minY}`]: "top",
-// 		[`${y === gridBoundaries.maxY}`]: "bottom",
-// 		[`${x === gridBoundaries.minX && middle}`]: "middleLeft",
-// 		[`${x === gridBoundaries.maxX && middle}`]: "middleRight"
-// 	}	
-// 	return keyObj[true]
-// }
-
-// // Return a string that we'll use to translate the shape
-// function calculateTransform(x,y) {	
-// 	const { cellHeight, cellWidth } = cellDimensions()
-// 	const middleRow = y > gridBoundaries.minY && y < gridBoundaries.maxY
-// 	let key = getTransformKey(x,y,middleRow)
-
-// 	const boardLocations = {
-// 		top: `0, ${viewportHeight + cellHeight}`, 
-// 		bottom: `0, -${viewportHeight + cellHeight}`,
-// 		middleLeft: `${viewportWidth + cellWidth}, 0`,
-// 		middleRight: `-${viewportWidth}, 0`
-// 	}
-
-// 	return `translate(${boardLocations[key]})`
-// }
-
-// function createParticles() {
-// 	const particleData = createParticleData()	
-// 	const particleColor = createColor()
-// 	const { cellHeight, cellWidth } = cellDimensions()
-
-// 	const circle = grid.selectAll(".particle")
-// 		.data(particleData)
-// 		.enter().append("circle")
-// 		.attr("cx", function(d) { return d.x })
-// 		.attr("cy", function(d) { return d.y })
-// 		.attr("r", getNumberInRange(5, 30))
-// 		.attr("fill", particleColor)
-// 		.attr("stroke", particleColor)
-// 		.transition()
-// 		.ease(d3.easeLinear)
-// 		.attr("transform", function(d) { return calculateTransform(d.x, d.y) })		
-// 		.duration(animationDuration)		
-	
-// 	circle.remove()	
-// }
-
-// function getNumberInRange(min, max) {
-// 	return Math.floor(Math.random() * (max - min + 1) + min);
-// }
-
-// // function runAnimation() {	
-// 	let previousTime = performance.now()
-	
-// 	const delay = getNumberInRange(animationDuration + 2, animationDuration * 7)
-	
-// 	function animateParticles(currentTime) {		
-// 		console.log("currentTime", currentTime, "previousTime", previousTime)
-// 		const delta = currentTime - previousTime
-// 		console.log("delta", delta, "delay", delay)
-
-// 		if (delta >= delay) {
-// 			console.log('%c Inside if statement ', 'background: skyblue; color: white; font-size: x-large');
-// 			previousTime = currentTime
-// 			createParticles()			
-// 		}
-// 		window.requestAnimationFrame(animateParticles)
-// 	}
-// 	animateParticles()
-// // }
-// // runAnimation()
