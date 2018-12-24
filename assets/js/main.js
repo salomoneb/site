f71d1883fab02ad8cb16607b3f331f7ef8e667f6
Salomone Baquis <salomone.l.baquis@gmail.com>
2018-12-24 13:21:56 -0500

    fixing broken colors on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ console.log("%c👋", "font-size:x-large")
 
 function createColor() {
   const hue = Math.round(Math.random() * 360)
-  return `hsla(${hue}, 80%, 65%)`
+  return `hsl(${hue}, 80%, 65%)`
 }
 const linkColor = createColor()
 document.querySelectorAll("a").forEach(function(link) {
